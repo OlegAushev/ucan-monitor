69f41b3c255f058a664ea7aed68a34dff38b4d28
Oleg Aushev <aushevom@gmail.com>
2023-09-25 20:25:13 +0300

    add .gitignore

--- build/generated/git_info.cpp
+++
@@ -1,5 +0,0 @@
-const char* GIT_DESCRIBE = "9702119";
-const char* GIT_HASH = "9702119";
-const char* GIT_BRANCH = "master";
-const char* GIT_DIFF = "+";
-
--- build/generated/git_info.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-
-extern const char* GIT_DESCRIBE;
-extern const char* GIT_DIFF;
-extern const char* GIT_HASH;
-extern const char* GIT_BRANCH;
-
