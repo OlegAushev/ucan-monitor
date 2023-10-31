c738ab1d953485db6c7fabb726dfa73c44cccbf3
Oleg Aushev <aushevom@gmail.com>
2023-11-01 00:29:14 +0300

    change server selector window

--- src/ui/serverselector/serverselector.cpp
+++ src/ui/serverselector/serverselector.cpp
@@ -8,13 +8,15 @@ void ServerSelector::show(const std::vector<std::string>& server_names) {
     ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoDocking
                                   | ImGuiWindowFlags_NoResize
                                   | ImGuiWindowFlags_NoMove
-                                  | ImGuiWindowFlags_NoTitleBar
+                                  //| ImGuiWindowFlags_NoTitleBar
                                   | ImGuiWindowFlags_NoCollapse
                                   | ImGuiWindowFlags_NoBringToFrontOnFocus
                                   | ImGuiWindowFlags_NoNavFocus;
     const ImGuiViewport* viewport = ImGui::GetMainViewport();
-    ImGui::SetNextWindowPos(viewport->WorkPos);
-    ImGui::SetNextWindowSize(viewport->WorkSize);
+    //ImGui::SetNextWindowPos(viewport->WorkPos);
+    //ImGui::SetNextWindowSize(viewport->WorkSize);
+    ImGui::SetNextWindowPos({(viewport->WorkSize.x - 400)/2, (viewport->WorkSize.y - 200)/2});
+    ImGui::SetNextWindowSize({400, 200});
     ImGui::SetNextWindowViewport(viewport->ID);
     
     ImGui::Begin("Select Server", nullptr, window_flags);
@@ -25,7 +27,7 @@ void ServerSelector::show(const std::vector<std::string>& server_names) {
 
     ImGui::NewLine();
 
-    if (ImGui::Button("Ok", ImVec2{50, 0})) {
+    if (ImGui::Button("Ok", ImVec2{100, 0})) {
         _server_is_selected = true;
         _selected_server = server_names[_selected_server_idx];
     }
