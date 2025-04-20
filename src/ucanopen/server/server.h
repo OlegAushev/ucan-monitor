fc8f684337278be0a0b31fe697d115e58bcb50b6
Oleg Aushev <aushevom@gmail.com>
2025-04-21 00:16:49 +0300

    add helper send method

--- src/ucanopen/server/server.h
+++ src/ucanopen/server/server.h
@@ -1,20 +1,18 @@
 #pragma once
 
-
-#include <ucanopen/server/impl/impl_server.h>
-#include <ucanopen/server/services/heartbeat/server_heartbeat_service.h>
-#include <ucanopen/server/services/tpdo/server_tpdo_service.h>
-#include <ucanopen/server/services/rpdo/server_rpdo_service.h>
-#include <ucanopen/server/services/watch/server_watch_service.h>
-#include <ucanopen/server/services/config/server_config_service.h>
-#include <ucanopen/server/services/sdo/server_sdo_service.h>
-#include <ucanopen/server/services/log/server_log_service.h>
-#include <ucanopen/server/utils/server_od_utils.h>
 #include <atomic>
-#include <functional>
 #include <chrono>
+#include <functional>
 #include <future>
-
+#include <ucanopen/server/impl/impl_server.h>
+#include <ucanopen/server/services/config/server_config_service.h>
+#include <ucanopen/server/services/heartbeat/server_heartbeat_service.h>
+#include <ucanopen/server/services/log/server_log_service.h>
+#include <ucanopen/server/services/rpdo/server_rpdo_service.h>
+#include <ucanopen/server/services/sdo/server_sdo_service.h>
+#include <ucanopen/server/services/tpdo/server_tpdo_service.h>
+#include <ucanopen/server/services/watch/server_watch_service.h>
+#include <ucanopen/server/utils/server_od_utils.h>
 
 namespace ucanopen {
 
@@ -29,9 +27,12 @@ public:
     ServerConfigService config_service;
     ServerLogService log_service;
 public:
-    Server(std::shared_ptr<can::Socket> socket, NodeId node_id, const std::string& name, const ObjectDictionary& dictionary);
+    Server(std::shared_ptr<can::Socket> socket,
+           NodeId node_id,
+           const std::string& name,
+           const ObjectDictionary& dictionary);
     virtual ~Server() = default;
-private:	
+private:
     void _send();
     void _handle_frame(const can_frame& frame);
     void _set_node_id(NodeId nodeId);
@@ -39,48 +40,69 @@ private:
     std::vector<impl::FrameReceiver*> _rx_services;
 
 public:
-    std::optional<std::string> read_string(std::string_view category,
-                                           std::string_view subcategory,
-                                           std::string_view name,
-                                           std::chrono::milliseconds timeout = std::chrono::milliseconds(1000));
-    
-    std::optional<std::string> read_scalar(std::string_view category,
-                                           std::string_view subcategory,
-                                           std::string_view name,
-                                           std::chrono::milliseconds timeout = std::chrono::milliseconds(1000));
-    
-    std::optional<ExpeditedSdoData> read_expdata(std::string_view category,
-                                                 std::string_view subcategory,
-                                                 std::string_view name,
-                                                 std::chrono::milliseconds timeout = std::chrono::milliseconds(1000));
-    
-    std::optional<ExpeditedSdoData> exec_expdata(std::string_view category,
-                                                 std::string_view subcategory,
-                                                 std::string_view name,
-                                                 std::chrono::milliseconds timeout = std::chrono::milliseconds(1000));
+    std::optional<std::string>
+    read_string(std::string_view category,
+                std::string_view subcategory,
+                std::string_view name,
+                std::chrono::milliseconds timeout =
+                        std::chrono::milliseconds(1000));
 
-    std::optional<ExpeditedSdoData> write_expdata(std::string_view category,
-                                                  std::string_view subcategory,
-                                                  std::string_view name,
-                                                  ExpeditedSdoData sdo_data,
-                                                  std::chrono::milliseconds timeout = std::chrono::milliseconds(1000));
+    std::optional<std::string>
+    read_scalar(std::string_view category,
+                std::string_view subcategory,
+                std::string_view name,
+                std::chrono::milliseconds timeout =
+                        std::chrono::milliseconds(1000));
 
-    ODAccessStatus read(std::string_view category, std::string_view subcategory, std::string_view name) {
+    std::optional<ExpeditedSdoData>
+    read_expdata(std::string_view category,
+                 std::string_view subcategory,
+                 std::string_view name,
+                 std::chrono::milliseconds timeout =
+                         std::chrono::milliseconds(1000));
+
+    std::optional<ExpeditedSdoData>
+    exec_expdata(std::string_view category,
+                 std::string_view subcategory,
+                 std::string_view name,
+                 std::chrono::milliseconds timeout =
+                         std::chrono::milliseconds(1000));
+
+    std::optional<ExpeditedSdoData>
+    write_expdata(std::string_view category,
+                  std::string_view subcategory,
+                  std::string_view name,
+                  ExpeditedSdoData sdo_data,
+                  std::chrono::milliseconds timeout =
+                          std::chrono::milliseconds(1000));
+
+    ODAccessStatus read(std::string_view category,
+                        std::string_view subcategory,
+                        std::string_view name) {
         return sdo_service.read(category, subcategory, name);
     }
 
-    ODAccessStatus write(std::string_view category, std::string_view subcategory, std::string_view name, ExpeditedSdoData sdo_data) {
+    ODAccessStatus write(std::string_view category,
+                         std::string_view subcategory,
+                         std::string_view name,
+                         ExpeditedSdoData sdo_data) {
         return sdo_service.write(category, subcategory, name, sdo_data);
     }
 
-    ODAccessStatus write(std::string_view category, std::string_view subcategory, std::string_view name, const std::string& value) {
+    ODAccessStatus write(std::string_view category,
+                         std::string_view subcategory,
+                         std::string_view name,
+                         const std::string& value) {
         return sdo_service.write(category, subcategory, name, value);
     }
-    
-    ODAccessStatus exec(std::string_view category, std::string_view subcategory, std::string_view name) {
+
+    ODAccessStatus exec(std::string_view category,
+                        std::string_view subcategory,
+                        std::string_view name) {
         return sdo_service.exec(category, subcategory, name);
     }
+public:
+    void send(can_frame const& frame) { _socket->send(frame); }
 };
 
 } // namespace ucanopen
-
